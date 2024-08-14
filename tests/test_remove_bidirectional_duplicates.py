9844206f86b2fd464ce65b206da6738fb820cfe3
Patrick Steinert <me@patrickbreucking.de>
2024-08-14 08:38:32 +0200

    linting

--- tests/test_remove_bidirectional_duplicates.py
+++ tests/test_remove_bidirectional_duplicates.py
@@ -8,6 +8,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src'
 from sgconversion import remove_bidirectional_duplicates, Tuple  # pylint: disable=wrong-import-position
 
 class TestConversion(unittest.TestCase):
+    """
+    Test for bidirectional duplicates removal.
+    """
+
     @patch('builtins.open')
     def test_filter_bidirectional(self, mock_open):
         """Test the filter_log_file function."""
@@ -23,7 +27,7 @@ class TestConversion(unittest.TestCase):
             (o_tuple.x == 'a' and o_tuple.y == 'left' and o_tuple.z == 'b') or
             (o_tuple.x == 'b' and o_tuple.y == 'right' and o_tuple.z == 'a')
         )
- 
+
     @patch('builtins.open')
     def test_filter_dublictaed(self, mock_open):
         """Test the filter_log_file function."""
@@ -38,7 +42,7 @@ class TestConversion(unittest.TestCase):
         self.assertTrue(
             o_tuple.x == 'a' and o_tuple.y == 'left' and o_tuple.z == 'b'
         )
- 
+
     @patch('builtins.open')
     def test_filter_single(self, mock_open):
         """Test the filter_log_file function."""
@@ -59,7 +63,8 @@ class TestConversion(unittest.TestCase):
         """Test the filter_log_file function."""
         # Mock the file content
         #in_tuples = [('a', 'left', 'b'), ('b', 'right', 'a')]
-        in_tuples = [Tuple('a', 'left', 'b'), Tuple('b', 'right', 'a'), Tuple('a', 'beneath', 'b'), Tuple('b', 'above', 'a')]
+        in_tuples = [Tuple('a', 'left', 'b'), Tuple('b', 'right', 'a'), 
+                     Tuple('a', 'beneath', 'b'), Tuple('b', 'above', 'a')]
 
         out_tuples = remove_bidirectional_duplicates(in_tuples)
 
